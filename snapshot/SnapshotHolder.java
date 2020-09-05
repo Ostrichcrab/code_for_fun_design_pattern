e091b1000228434ecf53143d6fb16f5d739d4761
Ostrichcrab <1447571721@qq.com>
2020-09-05 22:04:59 +0800

    snapshot

---
+++ snapshot/SnapshotHolder.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SnapshotHolder {
+    private Stack<Snapshot> snapshots = new Stack<>();
+  
+    public Snapshot popSnapshot() {
+      return snapshots.pop();
+    }
+  
+    public void pushSnapshot(Snapshot snapshot) {
+      snapshots.push(snapshot);
+    }
+  }
